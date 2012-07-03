7f191a85b2efd50ed5585b313795bd10421119c0
twbell <tyler@factual.com>
2012-07-03 13:19:49 -0700

    Heavy optimization, changes to format

--- categories/utils/php/json-to-csv.php
+++ categories/utils/php/json-to-csv.php
@@ -3,14 +3,16 @@
 /*
  * Created on Apr 17, 2012
  * Converts Factual JSON taxonomy to CSV file with format:
- * ID [delim] LABEL [delim] BREADCRUMB
+ * ID [delim] LABEL [delim] PARENT [delim] BREADCRUMB ARRAY
  * 
  * Abstract root node is output but not included in breadcrumbs
  * 
  */
 
+//Set error level
+error_reporting (E_ERROR);
+
 //change these to suit
-$breadCrumbDelimitor = " > "; //breadcrumb delimiter
 $delim = "\t"; //csv file delimiter
 $lang = "en"; //language to render
 $taxonomyFile = "../../factual_taxonomy.json"; //filename and path
@@ -23,18 +25,54 @@ $iteration = array ();
 //get information for each node
 foreach ($taxonomy as $nodeID => $node) {
 	$iteration[$nodeID]['label'] = $taxonomy[$nodeID]['labels'][$lang];
-	$iteration[$nodeID]['breadcrumb'] = getBreadCrumb($nodeID);
+	$iteration[$nodeID]['parents'] = $taxonomy[$nodeID]['parents'];
+	$iteration[$nodeID]['ancestors'] = getAncestors($nodeID);
+    $iteration[$nodeID]['breadcrumb'] = getBreadCrumb($nodeID);
 }
-
 //order by node ID
 ksort($iteration);
 
 //render to file
 $fp = fopen($outPutFile, 'a');
 foreach ($iteration as $nodeID => $node) {
-	fwrite($fp, $nodeID . $delim . $node['label'] . $delim . implode($breadCrumbDelimitor, $node['breadcrumb']) . "\n");
+	
+	$line =  $nodeID . $delim . 
+	         $node['label'] . $delim . 
+	         $node['parents'][0] . $delim. 
+	         json_encode($node['breadcrumb']) . "\n";
+
+	fwrite($fp, $line);
 }
 fclose($fp);
+
+//get ancestors
+function getAncestors($nodeID){
+	global $globalParents;
+	global $taxonomy;
+	if ($nodeID == 1){return array();} //catches root
+	if (isset ($globalParents[$nodeID])) {
+		$aBreadCrumb = $globalParents[$nodeID];
+		$globalParents[$nodeID] = $aBreadCrumb;
+	} else {
+		$iterationID = $nodeID;
+		while ($parent = getParent($iterationID)){
+			$aBreadCrumb[] = $parent;
+			$iterationID = $parent;			
+		}
+		$globalParents[$nodeID] = $aBreadCrumb;
+	}
+	return array_reverse($aBreadCrumb);	
+}
+
+
+function getParent($nodeID){
+	global $taxonomy;
+	if (isset($taxonomy[$nodeID]['parents'][0])){
+		return $taxonomy[$nodeID]['parents'][0];
+	} else {
+		return false;
+	}
+}
 
 /**
  * returns breadcrumb for a specific node. Works only for current monohierarchy.
@@ -46,25 +84,15 @@ function getBreadCrumb($nodeID) {
 	global $lang;
 	global $taxonomy;
 	global $iteration;
-	//iterate
-	foreach ($taxonomy[$nodeID]['parents'] as $parents) {
-		if (isset ($iteration[$taxonomy[$nodeID]['parents'][0]])) {
-			//spit back if parent has been completed previously
-			$aBreadCrumb = $iteration[$taxonomy[$nodeID]['parents'][0]]['breadcrumb'];
-			$aBreadCrumb[] = $taxonomy[$nodeID]['labels'][$lang];
-			return $aBreadCrumb;
-		} else {
-			$parent = $parents[0];
-			while ($parent) {
-				//recurse of course
-				$aBreadCrumb = getBreadCrumb($parent);
-				$parent = $taxonomy[$nodeID]['parents'][0];
-			}
-			$aBreadCrumb[] = $taxonomy[$nodeID]['labels'][$lang]; //add label to parent breadcrumb
-			return $aBreadCrumb;
+
+	if (isset($iteration[$nodeID]['ancestors'])){
+		array_shift($iteration[$nodeID]['ancestors']); //removes root node
+		foreach ($iteration[$nodeID]['ancestors'] as $ancestor){
+			$aBreadCrumb[] = $taxonomy[$ancestor]['labels'][$lang];
 		}
 	}
-	return array (); //catches root without parents
+	$aBreadCrumb[] = $taxonomy[$nodeID]['labels'][$lang];
+	return $aBreadCrumb;
 }
 
 exit;
